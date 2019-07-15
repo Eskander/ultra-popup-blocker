0a28171c8d40d18597d24bd66951087de30b960e
Eskander <skan.bejaoui@outlook.com>
2019-07-15 22:29:49 +0100

    Edit script header

--- ultra-popup-blocker.user.js
+++ ultra-popup-blocker.user.js
@@ -1,49 +1,28 @@
 // ==UserScript==
 // @name           Ultra Popup Blocker
-// @description    Configurable popup blocker that blocks ALL (even user-initiated) popup windows by default. You can easily open the blocked popup or whitelist a domain, directly from the page.
+// @description    Configurable popup blocker that blocks all popup windows by default. You can easily open the blocked popup or whitelist a domain, directly from the page.
 // @namespace      https://github.com/eskander
 // @author         eskander
 // @version        0.1
 // @include        *
+// @license        MIT
+// @homepage       https://github.com/eskander/ultra-popup-blocker
+// @supportURL     https://github.com/eskander/ultra-popup-blocker/issues
+// @compatible     firefox Tested with Tampermonkey
 // @grant          GM_getValue
 // @grant          GM_setValue
 // ==/UserScript==
 
-// Why another popup blocker?
-
-// Built-in Firefox popup blocker blocks only the popup that were created automatically via
-// script on page load etc. When you click on a button on the page, it won't block it.
-// However, malicious websites can create JS code that will launch the popups whenever you click
-// on any blank space on the page for instance.
-
-// In JavaScript, functions are first-order citizens. It means you can store a function in a variable
-// and pass it freely. You can also modify the native functions provided by the browser, like 'window.open'.
-// Here, we override 'window.open' with our own implementation which doesn't really open the window.
-// However the user may want to open a popup - cool, we stored original native window.open function
-// in a variable and we will call it!
-
-// Of course, we'd like to white list some domains. Greasemonkey offers an option to specify @exclude entries
-// in the metadata block of the script. However this means you'll have to edit this text file each time you
-// want to whitelist a domain. However, GM also offers a way to set config entries in Firefox storage
-// (about:config entries) and we'll make use of it to dynamically add entries to the white list 
-// right from the page.
-
-// The essence of the blocking code is substituting window.open with the function returning FakeWindow,
-// which can be a one-liner.
-// The majority of the code here is the handling of whitelisting logic and informational logging.
-
-// Tests: http://www.popuptest.com/
-
-(function() {
+(function () {
     // ============================== CONFIG =================================
     var bDisplayMessageOnPopupBlocked = true;
     var bDisplayOpenPopupLink = true;
     var bDisplayWhiteListThisDomainLink = true;
     var LOG_ID = "ultimate_popup_blocker"; // HTML ID in the page
-    
+
     // ============================ FUNCTIONS ================================
     var global = unsafeWindow; // reference to page's "window" through GreaseMonkey
-    
+
     /*
      * Helper to create a button with inner text @text executing onclick @clickCallback,
      * appended as a child of @logDiv
@@ -58,17 +37,17 @@
         logDiv.appendChild(button);
         button.addEventListener("click", clickCallback);
     };
-    
+
     /*
      * Helper to create a button (child of @logDiv) which onclick whitelists @domain
      * in internal Firefox storage.
      */
     var putWhitelistButton = function (logDiv, domain) {
-        putButton(logDiv, domain, function(){
+        putButton(logDiv, domain, function () {
             GM_setValue("whitelisted_" + domain, true);
         });
     };
-    
+
     /* 
      * Helper to create a text node with @text and append to @logDiv
      */
@@ -76,16 +55,16 @@
         var node = document.createTextNode(text);
         logDiv.appendChild(node);
     };
-    
+
     /*
      * Return logger div, or create it ad-hoc.
      */
     var getLogDiv = function () {
         var logDiv = document.getElementById(LOG_ID);
-        if(!logDiv){
+        if (!logDiv) {
             logDiv = document.createElement("div");
             logDiv.setAttribute("id", LOG_ID);
-            logDiv.style.cssText="position:fixed; bottom:0; left:0; z-index: 99999; width:100%;\
+            logDiv.style.cssText = "position:fixed; bottom:0; left:0; z-index: 99999; width:100%;\
                 padding:5px 5px 5px 29px; font: 8pt microsoft sans serif;\
                 background-color: linen; color:black; border:1px solid black;\
                 ";
@@ -93,7 +72,7 @@
         }
         return logDiv;
     };
-    
+
     /*
      * Get array of domains for which it would make sense to whitelist them.
      * Sample valid outputs:
@@ -102,42 +81,42 @@
      *  // www.youtube.com -> ['youtube.com', 'www.youtube.com']
      *  // a.b.c.d         -> ['c.d', 'b.c.d', 'a.b.c.d']
      */
-    var getDomainsArray = function(documentDomain){
+    var getDomainsArray = function (documentDomain) {
         // e.g. domain = www.google.com, topDomain = google.com
         var d1 = documentDomain;
         var domainsArr = [];
-        
+
         var lastDot1 = d1.lastIndexOf('.');
-        if(lastDot1 != -1){
-            var lastDot2 = d1.lastIndexOf('.', lastDot1-1);
-            if(lastDot2 != -1 && lastDot2 != lastDot1) {
+        if (lastDot1 != -1) {
+            var lastDot2 = d1.lastIndexOf('.', lastDot1 - 1);
+            if (lastDot2 != -1 && lastDot2 != lastDot1) {
                 var d2 = d1.substr(lastDot2 + 1);
                 domainsArr.push(d2);
-  
-                var lastDot3 = d1.lastIndexOf('.', lastDot2-1);
-                if(lastDot3 != -1 && lastDot3 != lastDot2) {
+
+                var lastDot3 = d1.lastIndexOf('.', lastDot2 - 1);
+                if (lastDot3 != -1 && lastDot3 != lastDot2) {
                     var d3 = d1.substr(lastDot3 + 1);
                     domainsArr.push(d3);
                 }
             }
         }
-        
+
         domainsArr.push(d1);
         return domainsArr;
     };
-    
+
     /*
      * Checks if domain we're currently browsing has been whitelisted by the user
      * to display popups.
      */
-    var isCurrentDomainWhiteListed = function() {
+    var isCurrentDomainWhiteListed = function () {
         var domains = getDomainsArray(document.domain);
-        var whitelisted = domains.some(function(d){
+        var whitelisted = domains.some(function (d) {
             return GM_getValue("whitelisted_" + d);
         }); // if any 'd' in 'domains' was whitelisted, we return true
         return whitelisted;
     };
-    
+
     /*
      * "window.open()" returns Window which might be then used by the originator page
      * to focus the popup (annoying splash popup) or blur it to retain focus in the original page
@@ -146,10 +125,14 @@
      * page wants to call focus() or blur().
      */
     var FakeWindow = {
-        blur: function() {return false;},
-        focus: function() {return false;}
+        blur: function () {
+            return false;
+        },
+        focus: function () {
+            return false;
+        }
     };
-    
+
     /*
      * Storing a reference to real "window.open" method in case we wanted
      * to actually open a blocked popup
@@ -161,23 +144,23 @@
      * if the blocker is activated.
      * We handle the blocking & messaging logic here.
      */
-    var fakeWindowOpen = function(url){
-        if(!bDisplayMessageOnPopupBlocked){
+    var fakeWindowOpen = function (url) {
+        if (!bDisplayMessageOnPopupBlocked) {
             return FakeWindow;
         }
         var logDiv = getLogDiv();
         logMessage(logDiv, url);
-        
-        if(bDisplayOpenPopupLink){
+
+        if (bDisplayOpenPopupLink) {
             displayOpenPopupLink(logDiv, arguments);
         }
-        if(bDisplayWhiteListThisDomainLink) {
+        if (bDisplayWhiteListThisDomainLink) {
             displayWhiteListThisDomainLink(logDiv);
         }
         displayCloseButton(logDiv);
         return FakeWindow; // see the doc of FakeWindow
     };
-    
+
     var logMessage = function (logDiv, url) {
         global.upb_counter = (global.upb_counter || 0);
         url = (url[0] == '/') ? document.domain + url : url;
@@ -186,45 +169,45 @@
         console.log(msg);
         logDiv.style.display = "block";
     };
-    
-    var displayOpenPopupLink = function (logDiv, realArguments){
-        putButton (logDiv, "open the popup", function(){
+
+    var displayOpenPopupLink = function (logDiv, realArguments) {
+        putButton(logDiv, "open the popup", function () {
             realWindowOpen.apply(null, realArguments);
         });
     };
-    
-    var displayWhiteListThisDomainLink = function(logDiv) {
+
+    var displayWhiteListThisDomainLink = function (logDiv) {
         var domainsArr = getDomainsArray(document.domain);
-        
+
         putText(logDiv, ' whitelist the domain: '); // using 'innerHTML += ' breaks event listeners strangely
         putWhitelistButton(logDiv, domainsArr[0]);
-        if(domainsArr[1]){
+        if (domainsArr[1]) {
             putWhitelistButton(logDiv, domainsArr[1]);
         }
-        if(domainsArr[2]){
+        if (domainsArr[2]) {
             putWhitelistButton(logDiv, domainsArr[2]);
         }
     };
-    
-    var displayCloseButton = function(logDiv) {
-        putButton (logDiv, "x", function(){
+
+    var displayCloseButton = function (logDiv) {
+        putButton(logDiv, "x", function () {
             logDiv.style.display = 'none';
         }, 'background-color: #a00; color:white; margin:0 32px 0 0; float:right');
     };
-    
+
     /*
      * Override browser's "window.open" with our own implementation.
      */
-    var activateBlocker = function() {
+    var activateBlocker = function () {
         global.open = fakeWindowOpen;
     };
-    
+
     // ============================ LET'S RUN IT ================================
-    
+
     var disabled = isCurrentDomainWhiteListed();
-    if(disabled){
+    if (disabled) {
         console.log('[UPB] current domain was found on a white list. UPB disabled.');
-    }else{
+    } else {
         activateBlocker();
     }
 })();
