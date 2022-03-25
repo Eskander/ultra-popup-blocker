68be05cc3172d4b293ff099baab13d917358a1d9
Eskander Bejaoui <mail@eskander.tn>
2022-03-25 09:49:23 +0100

    Update homepage URL

--- ultra-popup-blocker.user.js
+++ ultra-popup-blocker.user.js
@@ -6,7 +6,7 @@
 // @version      3.3
 // @include      *
 // @license      MIT
-// @homepage     https://eskander.github.io/ultra-popup-blocker/
+// @homepage     https://eskander.tn/ultra-popup-blocker/
 // @supportURL   https://github.com/Eskander/ultra-popup-blocker/issues/new
 // @compatible   firefox Tampermonkey recommended
 // @compatible   chrome Tampermonkey recommended
